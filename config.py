c5efecb0bc1621643a4d5f92a9ead89c5d2df605
arianmonti <a.m.ontazeri1385@gmail.com>
2021-10-28 12:27:53 +0330

    add database

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
 import os
 
+basepath = os.path.abspath(os.path.dirname(__file__))
+default_dbpath = os.path.join(basepath, 'app.db')
+
 
 def set_config(env_var: str, default: str) -> str:
     env = os.environ.get(env_var)
-    if env == '':
-        return ''
+    if not env:
+        return default
     return env
 
 
 class Cfg:
     SECRET_KEY = set_config('SECRET_KEY', 'secret')
+    SQLALCHEMY_DATABASE_URI = set_config(
+        'SQLALCHEMY_DATABASE_URI', f'sqlite:///{default_dbpath}'
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
