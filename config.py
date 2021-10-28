e582293a28ba3f4be4354fbce5f59602a938eb64
arianmonti <a.m.ontazeri1385@gmail.com>
2021-10-28 09:08:35 +0330

    add config

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+def set_config(env_var: str, default: str) -> str:
+    env = os.environ.get(env_var)
+    if env == '':
+        return ''
+    return env
+
+class Cfg:
+    SECRET_KEY = set_config('SECRET_KEY', 'secret')
